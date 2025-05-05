5b845f5d76b10a9b15c0ef1cbc3123ca4c48809e
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-05-05 22:58:04 +0300

    new updates

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 // docs/.vitepress/config.js
 export default {
-    base: '/',
+    base: '/docs/',
     title: 'Omniface Docs',
     description: 'Documentation for Omniface, Warp CLI, and useApi',
     
